82162dcb333843e7d0e6b46049fe36ed60b7281d
Juien <JuienLo@luoruiendeMacBook-Air.local>
2017-03-01 21:57:33 +0800

    ex7

---
+++ ex7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  1 21:52:43 2017
+
+@author: JuienLo
+"""
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+b = [i for i in a if i % 2 == 0]
+print (b)
